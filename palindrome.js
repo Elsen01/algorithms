59c1f17814293d56b6fb094c612e380044de0128
Elsen01 <elsen@gmail.com>
2022-12-07 23:39:34 +0400

    palindrome

--- palindrome.js
+++ palindrome.js
@@ -4,3 +4,12 @@ function palindrome(str) {
 }
 
 console.log(palindrome('palindrome'))
+
+//-----------------
+function palindromes(str) {
+    return str.split('').every((el,i) => {
+        return el === str[str.length - i - 1]
+    })
+}
+
+console.log(palindromes('palinderomes'))
