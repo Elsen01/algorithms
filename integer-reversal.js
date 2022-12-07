777d66c4c52895b1378e9365f46c955800978c7f
Elsen01 <elsen@gmail.com>
2022-12-08 00:17:27 +0400

    integer-reversal

--- integer-reversal.js
+++ integer-reversal.js
@@ -4,4 +4,17 @@ function intReversal(int) {
     return parseInt(result) * Math.sign(int)
 }
 
-console.log(intReversal(-90))
+console.log(intReversal(-100))
+
+//-----------------
+
+function reversal(num) {
+    const intRevers = num.toString().split('').reverse().join('')
+
+if (num < 0) {
+    return parseInt(intRevers) * -1
+}
+return parseInt(intRevers)
+}
+
+console.log(reversal(411))
