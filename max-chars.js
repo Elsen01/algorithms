12cea815e9ae62b27a9bcc2a237c89fd01ed6a30
Elsen01 <elsen@gmail.com>
2022-12-08 00:43:25 +0400

    max-chars

--- max-chars.js
+++ max-chars.js
@@ -9,7 +9,34 @@ function maxChar(str) {
             char[el] =1
         }
     }
-    console.log(char)
+return char
 }
 
-maxChar('max char')
+console.log(maxChar('max char'))
+
+//------------------------
+
+function maxChars(str) {
+
+    const char = []
+    let max = 0;
+    let charMap = '';
+
+    for(let el of str) {
+        if(char[el]) {
+            char[el]++
+        }else {
+            char[el] =1
+        }
+    }
+
+    for (let el in char){
+        if (char[el] > max) {
+            max = char[el]
+            charMap = el
+        }
+    }
+    return charMap
+}
+
+console.log(maxChars('Programming'))
