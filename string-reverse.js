38da0032ca4d536107d6ca550248cec9c87069a3
Elsen01 <elsen@gmail.com>
2022-12-07 23:23:24 +0400

    string-reverse

--- string-reverse.js
+++ string-reverse.js
@@ -1,4 +1,9 @@
+function reverse(str) {
+    const result = str.split('').reverse().join('')
+    return result;
+}
 
+console.log(reverse('Hello World'))
 
 function reversed(str) {
     const revers = str.split('').reduce((curr,val)=> val + curr,'');
