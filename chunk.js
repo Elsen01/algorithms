a2ce536ec67dd06693f3b8ee126080b7864e5e80
Elsen01 <elsen@gmail.com>
2022-12-08 23:19:57 +0400

    chunk

--- chunk.js
+++ chunk.js
@@ -14,3 +14,18 @@ function chunk(array,size) {
 }
 
 console.log(chunk([1,2,3,4,5],2))
+
+//---------------------------
+
+function chunked(arr, size) {
+    const chunk = [];
+    let index = 0;
+
+    while (index < arr.length) {
+        chunk.push(arr.slice(index, index + size))
+        index += size
+    }
+    return chunk
+}
+
+console.log(chunked([1,2,3,4,5,55,6,7,8,9],3))
